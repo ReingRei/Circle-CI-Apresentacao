35c44d8c3d465dc49f159c9d7f80db77fd0b26cb
Reinaldo Filho <reinaldofilho76@gmail.com>
2023-11-16 10:58:55 -0300

    feat: Commit inicial

---
+++ test/product.test.js
@@ -0,0 +1,8 @@
+import Product from "../src/model/product";
+import sellProduct from "../src/service/sellProduct";
+
+test('deve validar baixa de estoque da venda de uma unidade', () => {
+    let produto = new Product('Celular', 500.00, 900.00, 10);
+    sellProduct(produto, 1);
+    expect(produto.stock).toBe(9);
+})
