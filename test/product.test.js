d280898f45e81872ad6303483990b321a03caa84
Reinaldo Filho <reinaldofilho76@gmail.com>
2023-11-16 12:31:03 -0300

    wip

--- test/product.test.js
+++ test/product.test.js
@@ -6,3 +6,9 @@ test('deve validar baixa de estoque da venda de uma unidade', () => {
     sellProduct(produto, 1);
     expect(produto.stock).toBe(9);
 })
+
+test('deve aceita a venda de mais de uma  unidade', () => {
+    let produto = new Product('Celular', 500.00, 900.00, 10);
+    sellProduct(produto, 3);
+    expect(produto.stock).toBe(7);
+})
